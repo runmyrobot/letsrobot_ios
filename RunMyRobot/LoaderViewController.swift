06486e25e581b52c4dcd25af400bc3036159fb62
James Sherlock <James.Sherlock@sky.uk>
2017-06-13 14:43:30 +0100

    Add forced/recommended update messages

--- RunMyRobot/LoaderViewController.swift
+++ RunMyRobot/LoaderViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import PopupDialog
 
 class LoaderViewController: UIViewController {
 
@@ -19,15 +20,74 @@ class LoaderViewController: UIViewController {
 
         progressLabel.text = "Downloading Config"
         
-        Networking.requestJSON("/internal") { [weak self] response in
+        Alamofire.request("http://sherlo.uk/letsrobot/config.json").validate().responseJSON { [weak self] response in
             guard let rawJSON = response.result.value else {
                 self?.progressLabel.text = "Error Downloading Config"
                 print("Something went wrong!")
                 return
             }
             
+            self?.progressLabel.text = "Parsing Config"
             let json = JSON(rawJSON)
-            self?.progressLabel.text = "Converting Config"
+            
+            let appId = json["appStoreId"].stringValue
+            
+            let appStore = DefaultButton(title: "Go to App Store", dismissOnTap: false) {
+                if let url = URL(string: "itms-apps://itunes.apple.com/app/id\(appId)"), UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+            
+            let forcedEnabed = json["forcedUpdate", "enabled"].boolValue
+            if forcedEnabed, self?.needsUpdate(version: json["forcedUpdate", "minimum_version"].stringValue) == true {
+                self?.progressLabel.text = "Outdated App"
+                
+                let dialog = PopupDialog(
+                    title: json["forcedUpdate", "title"].string,
+                    message: json["forcedUpdate", "message"].string,
+                    gestureDismissal: false
+                )
+                
+                dialog.addButtons([appStore])
+                
+                self?.present(dialog, animated: true, completion: nil)
+                return
+            }
+            
+            let recommendedEnabled = json["recommendedUpdate", "enabled"].boolValue
+            if recommendedEnabled, self?.needsUpdate(version: json["recommendedUpdate", "minimum_version"].stringValue) == true {
+                let dialog = PopupDialog(
+                    title: json["forcedUpdate", "title"].string,
+                    message: json["forcedUpdate", "message"].string,
+                    gestureDismissal: false
+                )
+                
+                let proceed = CancelButton(title: "Continue Anyway") {
+                    self?.loadRobots()
+                }
+                
+                dialog.addButtons([appStore, proceed])
+                
+                self?.present(dialog, animated: true, completion: nil)
+                return
+            }
+            
+            self?.loadRobots()
+        }
+    }
+    
+    private func loadRobots() {
+        progressLabel.text = "Downloading Robots"
+        
+        Networking.requestJSON("/internal") { [weak self] response in
+            guard let rawJSON = response.result.value else {
+                self?.progressLabel.text = "Error Downloading Robots"
+                print("Something went wrong!")
+                return
+            }
+            
+            let json = JSON(rawJSON)
+            self?.progressLabel.text = "Converting Robots"
             let config = Config(json: json)
             Config.shared = config
             
@@ -53,7 +113,7 @@ class LoaderViewController: UIViewController {
         }
     }
     
-    func startSocket() {
+    private func startSocket() {
         progressLabel.text = "Connecting Socket"
         
         Socket.shared.start { [weak self] _ in
@@ -63,6 +123,11 @@ class LoaderViewController: UIViewController {
                 self?.performSegue(withIdentifier: "EnterApp", sender: nil)
             }
         }
+    }
+    
+    private func needsUpdate(version: String) -> Bool {
+        guard let current = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else { return false }
+        return current.compare(version, options: .numeric) == .orderedAscending
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
