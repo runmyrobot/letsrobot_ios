1442e9972e62255b2706abd59685aca117690c32
James Sherlock <James.Sherlock@sky.uk>
2017-06-27 19:06:42 +0100

    Add form boilerplate to register

--- RunMyRobot/RegisterForm.swift
+++ RunMyRobot/RegisterForm.swift
@@ -7,10 +7,30 @@
 //
 
 import UIKit
+import GSMessages
 
 class RegisterForm: UIView {
 
+    @IBOutlet var usernameLabelCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var usernameIndicator: UIView!
+    
+    @IBOutlet var passwordLabelCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var passwordLabel: UILabel!
+    @IBOutlet var passwordIndicator: UIView!
+    
+    @IBOutlet var emailLabelCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var emailIndicator: UIView!
+    
+    @IBOutlet var registerButton: UIButton!
+    @IBOutlet var registerIndicator: UIActivityIndicatorView!
+    
     weak var parent: UIViewController?
+    let errorColour = GSMessage.errorBackgroundColor
     
     class func create(parent: UIViewController) -> RegisterForm {
         let views = UINib(nibName: "LoginView", bundle: nil).instantiate(withOwner: self, options: nil) as? [UIView]
@@ -24,4 +44,88 @@ class RegisterForm: UIView {
         return view
     }
 
+    @IBAction func didPressRegister() {
+        
+    }
+    
+    @IBAction func didStartEditingField(_ sender: UITextField) {
+        select(field: sender)
+    }
+    
+    func clearError(field: UITextField) {
+        UIView.animate(withDuration: 0.3) {
+            switch field.tag {
+            case 1:
+                self.usernameLabel.textColor = .white
+                self.usernameIndicator.backgroundColor = .white
+                break
+            case 2:
+                self.passwordLabel.textColor = .white
+                self.passwordIndicator.backgroundColor = .white
+                break
+            case 3:
+                self.emailLabel.textColor = .white
+                self.emailIndicator.backgroundColor = .white
+                break
+            default:
+                break
+            }
+        }
+    }
+    
+    func select(field: UITextField) {
+        let isUsername = field.tag == 1
+        
+        switch field.tag {
+        case 1:
+            usernameLabelCenterYConstraint.isActive = false
+            if fieldValue(for: passwordField) == nil {
+                passwordLabelCenterYConstraint.isActive = true
+            }
+            
+            UIView.animate(withDuration: 0.3) {
+                self.layoutIfNeeded()
+                self.usernameLabel.alpha = 1
+                self.usernameIndicator.alpha = 1
+                
+                if self.fieldValue(for: self.passwordField) == nil && self.passwordLabel.textColor != self.errorColour {
+                    self.passwordLabel.alpha = 0.3
+                    self.passwordIndicator.alpha = 0.3
+                }
+            }
+            break
+        case 2:
+            passwordLabelCenterYConstraint.isActive = false
+            if fieldValue(for: usernameField) == nil {
+                usernameLabelCenterYConstraint.isActive = true
+            }
+            
+            UIView.animate(withDuration: 0.3) {
+                self.layoutIfNeeded()
+                self.passwordLabel.alpha = 1
+                self.passwordIndicator.alpha = 1
+                
+                if self.fieldValue(for: self.usernameField) == nil && self.usernameLabel.textColor != self.errorColour {
+                    self.usernameLabel.alpha = 0.3
+                    self.usernameIndicator.alpha = 0.3
+                }
+            }
+            break
+        case 3:
+            
+            break
+        default:
+            break
+        }
+    }
+    
+    func fieldValue(for field: UITextField) -> String? {
+        let rawText = field.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if rawText == "" {
+            return nil
+        }
+        
+        return rawText
+    }
 }
