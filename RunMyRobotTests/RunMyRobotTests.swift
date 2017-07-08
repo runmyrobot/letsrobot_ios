43581cf542229f7fdf9d19d37a46c91eca776498
James Sherlock <James.Sherlock@sky.uk>
2017-07-08 18:52:03 +0100

    Added empty unit tests target

---
+++ RunMyRobotTests/RunMyRobotTests.swift
@@ -0,0 +1,28 @@
+//
+//  RunMyRobotTests.swift
+//  RunMyRobotTests
+//
+//  Created by Sherlock, James on 08/07/2017.
+//  Copyright © 2017 Let's Robot. All rights reserved.
+//
+
+import XCTest
+
+class RunMyRobotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
